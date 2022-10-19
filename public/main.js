f7a0ac44acaae6e15b93a987dd3a6c651e557106
Amadí Thiam <amadi.thiam@gmail.com>
2022-10-19 16:18:45 -0400

    Stormlight Archive

---
+++ public/main.js
@@ -0,0 +1,35 @@
+let kaladin = document.querySelector('img')
+
+const helloAlert = evt => {
+    alert("Welcome to Roshar! Storms, it's good to have you.")
+}
+kaladin.addEventListener('click', helloAlert)
+
+let shallan = document.querySelector('h1')
+
+const helloAlert1 = evt => {
+    alert("Bridge runs and beef stew, that's what Bridge Four does!")
+}
+shallan.addEventListener('click', helloAlert1)
+
+let parshendi = document.querySelector('h2')
+
+const helloAlert2 = evt => {
+    alert("The rythyms of the old songs have escaped us.")
+}
+parshendi.addEventListener('click', helloAlert2)
+
+let chasms = document.querySelector('h3')
+
+const helloAlert3 = evt => {
+    alert("Being caught on the Shattered Plains during a Highstorm meant almost certain death. Between chasm duty and bridge runs, a bridgeman's life was a fragile thing.")
+}
+chasms.addEventListener('click', helloAlert3)
+
+let stormlight = document.querySelector('h4')
+
+const helloAlert4 = evt => {
+    alert("Kaladin never knew he could feel so free - running through the chasms - bridgeman or not - he felt unstoppable.")
+}
+stormlight.addEventListener('click', helloAlert4)
+
